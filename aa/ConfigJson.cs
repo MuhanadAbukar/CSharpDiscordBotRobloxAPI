bc6e76604da4dc82a08d02bdd72feb02e6c5c3ce
Hanad <muhanadisnotabebe@gmail.com>
2022-10-11 08:41:48 +0200

    Add project files.

---
+++ aa/ConfigJson.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aa
+{
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+        [JsonProperty("prefix")]
+        public string Prefix { get; set; }
+    }
+}
